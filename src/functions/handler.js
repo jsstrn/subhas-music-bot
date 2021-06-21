632e1a1610b2501d57e351406b46df16e9264dcf
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-06-21 22:13:15 +0800

    Deploy after tests pass

--- src/functions/handler.js
+++ src/functions/handler.js
@@ -2,7 +2,7 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Your function executed successfully!",
+      message: "Your function was deployed via CI/CD and executed successfully!",
     }),
   };
 };
