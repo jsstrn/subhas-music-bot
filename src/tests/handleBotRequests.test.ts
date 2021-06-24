5678d4d798b7f5b1feda1a4a32dcac6d69c5c2ad
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-06-24 10:04:26 +0800

    Update test cases

--- src/tests/handleBotRequests.test.ts
+++ src/tests/handleBotRequests.test.ts
@@ -4,17 +4,18 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 
 jest.mock("../constants");
 
-describe("Handle Bot Requests", () => {
-  const handleUpdate = jest.spyOn(bot, "handleUpdate");
-  handleUpdate.mockName("handleUpdate");
+const handleUpdate = jest.spyOn(bot, "handleUpdate");
+handleUpdate.mockName("handleUpdate");
 
+const event = {} as APIGatewayProxyEvent;
+event.body = JSON.stringify({message: 'some request'});
+
+describe("Handle Bot Requests", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
   it("returns status code of 200 upon success", async () => {
-    const event = {} as APIGatewayProxyEvent;
-    event.body = JSON.stringify("");
     handleUpdate.mockResolvedValueOnce();
 
     const { statusCode } = await handleBotRequests(event);
@@ -23,8 +24,6 @@ describe("Handle Bot Requests", () => {
   });
 
   it("returns status code 500 upon failure", async () => {
-    const event = {} as APIGatewayProxyEvent;
-    event.body = JSON.stringify("");
     handleUpdate.mockRejectedValueOnce(new Error("Something went wrong"));
 
     const { statusCode } = await handleBotRequests(event);
@@ -33,12 +32,18 @@ describe("Handle Bot Requests", () => {
   });
 
   it("calls handleUpdate only once", async () => {
-    const event = {} as APIGatewayProxyEvent;
-    event.body = JSON.stringify("");
     handleUpdate.mockResolvedValueOnce();
 
     await handleBotRequests(event);
 
     expect(handleUpdate).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls handleUpdate with correct arguments", async () => {
+    handleUpdate.mockResolvedValueOnce();
+
+    await handleBotRequests(event);
+
+    expect(handleUpdate).toHaveBeenCalledWith({message: "some request"});
   });
 });
