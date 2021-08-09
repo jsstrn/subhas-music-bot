fd48e320239e0273300625842bee58dc308fbe51
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-08-09 13:58:51 +0800

    Rename template to match

--- src/bot/features/help/help.ts
+++ src/bot/features/help/help.ts
@@ -3,7 +3,7 @@ import { content } from "../../content";
 import { help, menu } from "../../props/commands";
 
 export default Composer.command(help, async (ctx) => {
-  const text = content("menu")({ menu });
+  const text = content("help")({ menu });
 
   await ctx.replyWithHTML(text);
 });
