0e5fc6930cc091730d9659e5884d2a8193621d1a
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-08-10 16:19:41 +0800

    Log errors

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,6 +2,7 @@ import { Telegraf, Telegram } from "telegraf";
 import { TELEGRAM_BOT_TOKEN } from "../constants";
 import { menu } from "./props/commands";
 import features from "./features";
+import { log } from "../util";
 
 if (!TELEGRAM_BOT_TOKEN) {
   throw new Error("[ERROR] Telegram bot token is required.");
@@ -14,3 +15,7 @@ export const telegram = new Telegram(TELEGRAM_BOT_TOKEN);
 telegram.setMyCommands(menu);
 
 bot.use(features);
+
+bot.catch((err) => {
+  log.error(err);
+});
