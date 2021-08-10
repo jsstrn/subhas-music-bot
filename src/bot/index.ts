7731ed84f50746c6c3635841d27483908b0e6062
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-08-10 16:07:32 +0800

    Remove bot error handling

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,9 +1,7 @@
 import { Telegraf, Telegram } from "telegraf";
 import { TELEGRAM_BOT_TOKEN } from "../constants";
-import { content } from "./content";
 import { menu } from "./props/commands";
 import features from "./features";
-import { log } from "../util";
 
 if (!TELEGRAM_BOT_TOKEN) {
   throw new Error("[ERROR] Telegram bot token is required.");
@@ -16,10 +14,3 @@ export const telegram = new Telegram(TELEGRAM_BOT_TOKEN);
 telegram.setMyCommands(menu);
 
 bot.use(features);
-
-const text = content("error")();
-
-bot.catch((err, ctx) => {
-  log.error(err);
-  ctx.reply(text);
-});
