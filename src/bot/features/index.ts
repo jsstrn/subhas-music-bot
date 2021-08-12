157eb0a45643d0cbe1750dc33cb724dd7743a18b
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-08-12 17:06:26 +0800

    Reorder bot features alphabetically

--- src/bot/features/index.ts
+++ src/bot/features/index.ts
@@ -1,20 +1,20 @@
 import { Composer } from "telegraf";
-import start from "./start";
-import help from "./help";
 import about from "./about";
-import version from "./version";
 import albums from "./albums";
 import error from "./error";
+import help from "./help";
 import order from "./order";
 import support from "./support";
+import start from "./start";
+import version from "./version";
 
 export default Composer.compose([
-  start,
-  help,
   about,
   albums,
   error,
+  help,
   order,
   support,
+  start,
   version,
 ]);
