0401a529dccfb9276b387dd8649edfc4aca58c62
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-08-12 18:01:31 +0800

    Refactor order process and fulfillment

--- src/bot/features/order/fulfillOrder.ts
+++ src/bot/features/order/fulfillOrder.ts
@@ -3,19 +3,10 @@ import { File } from "../../../db/schema";
 import { Album } from "../../../models/Album";
 import { content } from "../../content";
 
-export const fulfillOrder = async (ctx: Context): Promise<void> => {
-  // @ts-ignore
-  const data = ctx.message.successful_payment;
-  const albumId = data.invoice_payload;
-
+const sendAlbumInfo = async (ctx: Context, albumId: string) => {
   const album = Album.getById(albumId);
 
   const { cover, title, artist, description, lyrics, credits, tracks } = album;
-
-  const files = Album.getAllTrackFiles(albumId, {
-    source: "web",
-    category: "preview",
-  });
 
   const caption = content("album")({
     title,
@@ -30,8 +21,31 @@ export const fulfillOrder = async (ctx: Context): Promise<void> => {
     caption,
     parse_mode: "HTML",
   });
+};
+
+const sendTrackFiles = (ctx: Context, albumId: string) => {
+  const files = Album.getAllTrackFiles(albumId, {
+    mime: "audio/mpeg",
+    category: "preview",
+  });
 
   files.forEach(async (file: File) => {
     await ctx.replyWithAudio(file.id);
   });
+};
+
+const sendReceipt = async (ctx: Context) => {
+  ctx.reply("[TODO] Send receipt");
+};
+
+export const fulfillOrder = async (ctx: Context): Promise<void> => {
+  // @ts-ignore
+  const data = ctx.message.successful_payment;
+  const albumId = data.invoice_payload;
+
+  console.log("fulfullOrder", data);
+
+  await sendAlbumInfo(ctx, albumId);
+  await sendTrackFiles(ctx, albumId);
+  await sendReceipt(ctx);
 };
--- src/bot/features/order/processOrder.ts
+++ src/bot/features/order/processOrder.ts
@@ -1,8 +1,6 @@
 import { Context } from "telegraf";
 
 export const processOrder = async (ctx: Context): Promise<void> => {
-  console.log(ctx.preCheckoutQuery);
-
   const isSuccessfulPayment = true;
   const failedPaymentMessage = `😔  Sorry, your payment failed to go through.`;
 
